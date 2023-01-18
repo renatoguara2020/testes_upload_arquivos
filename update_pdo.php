cbb28218e2dc28816f6e8d802f031e84ca167d68
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-18 17:05:14 -0300

    ok

--- update_pdo.php
+++ update_pdo.php
@@ -9,9 +9,12 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  $stmt = $conn->prepare("UPDATE alunos SET lastname='Doe'  WHERE id=2 ");
+  $stmt = $conn->prepare("UPDATE alunos SET lastname= :lastname AND firstname= :firstname  WHERE id=:id ");
 
   // Prepare statement
+  $stmt->bindParam(':firstname', $firstname, PDO::PARAM_STR);
+  $stmt->bindParam(':lastname', $lastname, PDO::PARAM_STR);
+  $stmt->bindParam(':id', $id, PDO::PARAM_INT);
   $stmt->execute();
 
   // execute the query
@@ -22,7 +25,7 @@ try {
 } catch(PDOException $e) {
   echo  "<br>" . $e->getMessage() ." ". $e->getTraceAsString();
 }
-  while($row = $stmt->fetchAll()) {
+  while($row = $stmt->fetchAll(PDO::FETCH_ASSOC)) {
     echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</td></tr>";
 }
 
