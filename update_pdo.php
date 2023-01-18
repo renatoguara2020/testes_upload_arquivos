f632bfea2c82f94b2d2b5bb374435d0cd312eb62
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-18 16:47:57 -0300

    OK

--- update_pdo.php
+++ update_pdo.php
@@ -8,11 +8,12 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+  $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   $sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
+  $stmt = $conn->prepare("UPDATE alunos SET lastname='Doe'  WHERE id=2 ");
 
   // Prepare statement
-  $stmt = $conn->prepare($sql);
+  $stmt->execute();
 
   // execute the query
   $stmt->execute();
